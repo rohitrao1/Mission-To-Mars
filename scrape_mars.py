a7984b92847b068ab586f229c88b89ebe8f9107a
Rohit Rao <ro.rao2007@gmail.com>
2018-11-09 16:14:17 -0500

    Finalized my code

--- scrape_mars.py
+++ scrape_mars.py
@@ -4,13 +4,16 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup as bs 
 import requests
-#import pymongo
 from splinter import Browser
 
-executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
-browser = Browser('chrome', **executable_path, headless=False)
+# Initialize browser
+def init_browser():
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    #executable_path = {'executable_path': 'chromedriver.exe'}
+    return Browser("chrome", **executable_path, headless=False)
 
 def scrape():
+    browser = init_browser()
     url = 'https://mars.nasa.gov/news/'
     browser.visit(url)
     html = browser.html
@@ -23,8 +26,8 @@ def scrape():
     html = browser.html
     soup = bs(html, 'html.parser')
     base_url = 'https://www.jpl.nasa.gov'
-    featured_image_url = soup.find("a", class_ = "button fancybox")["data-fancybox-href"]
-    full_url = base_url + featured_image_url
+    image_url = soup.find("a", class_ = "button fancybox")["data-fancybox-href"]
+    featured_image_url = base_url + image_url
 
     url = 'https://twitter.com/marswxreport?lang=en'
     browser.visit(url)
@@ -38,6 +41,7 @@ def scrape():
     facts_df = tables[0]
     facts_df.columns = ['Fact', 'Value']
     facts_df['Fact'] = facts_df['Fact'].str.replace(':', '')
+    facts_df.reset_index(drop=True, inplace=True)
     facts_html = facts_df.to_html()
 
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
@@ -45,25 +49,26 @@ def scrape():
     html = browser.html
     soup = bs(html, 'html.parser')
     results = soup.find_all('div', class_="description")
+    base_url = 'https://astrogeology.usgs.gov/'
     sites = []
     for result in results:
         link = result.find('a', class_ = "itemLink product-item")
         link_text = link['href']
-        full_url = base_url + link_text
-        sites.append(full_url)
+        hemispheres_url = base_url + link_text
+        sites.append(hemispheres_url)
     hemispheres = []
     for site in sites:
         browser.visit(site)
         html = browser.html
         soup = bs(html, 'html.parser')
         title = soup.find('h2', class_ = "title").text.strip()
-        url = soup.find_all('a', target = "_blank", href = True)[1]['href']
+        url = soup.find_all('a', target = "_blank", href = True)[0]['href']
         hemispheres.append({"title": title, "img_url": url})
     
     output = {
         "news_title": news_title, 
         "news_p": news_p, 
-        "featured_image_url": full_url,
+        "featured_image_url": featured_image_url,
         "mars_weather": mars_weather, 
         "facts_html": facts_html,
         "hemispheres": hemispheres
